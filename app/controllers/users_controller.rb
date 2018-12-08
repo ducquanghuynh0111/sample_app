948c3c34f6de0c1db7e85f7c77817e68a7dd9e57
Quang <ducquanghuynh0111@gmail.com>
2018-12-08 15:04:08 +0700

    Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
+  def show; end
+
   def new
     @user = User.new
   end
 
-  def show; end
-
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t ".sign_up_success"
       redirect_to @user
     else
@@ -16,14 +17,14 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit :name, :email,
-      :password, :password_confirmation
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 
   def load_user
     @user = User.find_by id: params[:id]
     return if @user
-    flash[:danger] = t ".error_messages"
+    flash[:danger] = t ".load_user.error_messages"
     redirect_to root_path
   end
 end
